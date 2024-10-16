c39f7817311854887a5b40a1cb0a0f07639aceea
WilliamKSilva <wilz.kelvin12@gmail.com>
2024-10-15 22:11:16 -0300

    fix: rename lua-from-c.c file

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gcc -I/opt/lua-5.4.7/src -L/opt/lua-5.4.7/src -Wall -o lua-from-c lua-from-c.c -llua -ldl -lm
+gcc -I/opt/lua-5.4.7/src -L/opt/lua-5.4.7/src -Wall -o lua_from_c lua_from_c.c -llua -ldl -lm
